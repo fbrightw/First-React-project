c1fef5d84e99f6e08298f64f2bb9adc627cfdd97
Finch Brightwing <fbrightw@il-f5.msk.21-school.ru>
2022-09-14 17:08:25 +0300

    added webpack to build project

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Импортируем плагин
+
+let mode = 'development'; // По умолчанию режим development
+if (process.env.NODE_ENV === 'production') { // Режим production, если
+                                             // при запуске вебпака было указано --mode=production
+    mode = 'production';
+}
+
+const plugins = [
+    new HtmlWebpackPlugin({
+        template: './src/index.html', // Данный html будет использован как шаблон
+    }),
+]; // Создаем массив плагинов
+
+module.exports = {
+    mode, // Сокращенная запись mode: mode в ES6+
+    plugins,
+    entry: './src/index.js', // Указываем точку входа - главный модуль приложения,
+    // в который импортируются все остальные
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Директория, в которой будет
+        // размещаться итоговый бандл, папка dist в корне приложения
+
+        assetModuleFilename: 'assets/[hash][ext][query]', // Все ассеты будут
+        // складываться в dist/assets
+
+        clean: true, // Очищает директорию dist перед обновлением бандла
+        // Свойство стало доступно с версии 5.20.0, до этого использовался
+        // CleanWebpackPlugin
+    },
+
+    devtool: 'source-map',
+
+    devServer: {
+        hot: true, // Включает автоматическую перезагрузку страницы при изменениях
+    },
+
+    module: {
+        rules: [
+            { test: /\.(html)$/, use: ['html-loader'] },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, // не обрабатываем файлы из node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true, // Использование кэша для избежания рекомпиляции
+                        // при каждом запуске
+                    },
+                },
+            }, // Добавляем загрузчик для html
+        ],
+
+    }
+
+}
