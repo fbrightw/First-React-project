cb7e860fc85954edd8ff71fd1f0743f243b82085
fbrightw <gasanova_1998@bk.ru>
2022-09-27 21:30:02 +0300

    made Home component

--- src/Components/Sections/index.js
+++ src/Components/Sections/index.js
@@ -1,23 +1,20 @@
 import React, {Component} from 'react';
 import {ParticlesBackgroundStyled} from "../Body/ParticlesBackground";
-import Animated from "../Body/Animated.js";
 import Todos from "../Body/Todos.jsx";
 import './index.css'
+import Home from "./Home";
 
 export default class Sections extends Component {
     render() {
         return (
-            <div>
-                <section className="home" id="home">
-                    <ParticlesBackgroundStyled />
-                </section>
-                <section className='reading' id='reading'>
-                    <h1>Reading</h1>
-                </section>
+            <div className="Sections">
+                <ParticlesBackgroundStyled />
+                <Home />
                 <section className='toDo' id='toDo'>
-                    <div className="todoList"><Todos /></div>
+                    <div className="todoList">
+                        <Todos />
+                    </div>
                 </section>
-                {/*<Animated />*/}
                 {/*<div className="calendar">*/}
                 {/*     <Calendar />*/}
                 {/*    <div className="todoList"><Todos/></div>*/}
