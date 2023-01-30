cef5fbc1399545cf5ef87adbfca640f39e734768
fbrightw <gasanova_1998@bk.ru>
2023-01-30 21:38:29 +0300

    * second test is not ready but passes

--- src/__tests__/TaskContainer.test.js
+++ src/__tests__/TaskContainer.test.js
@@ -6,3 +6,11 @@ test('renders "no tasks" when there is no tasks', async () => {
   const {getByText} = render(<TaskContainer/>)
   expect(getByText((/no tasks/i))).toBeInTheDocument()
 })
+
+test('render list of tasks', () => {
+  const fakeList = [
+      {id: 0, text: 'first', subTasks: []}, {id: 1, text: 'second', subTasks: []}
+  ]
+  render(<TaskContainer tasks={false}/>)
+
+})
